5d10bcfdb6c122d09154da36d378767078f8dd6e
Michael Sorhaindo <michael@exmachina.nl>
2019-11-20 14:01:38 +0100

    attempt XGBoost regression

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # import numpy as np
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from xgboost.sklearn import XGBRegressor
+from ml_metrics import rmsle
 
 print('Script running..')
 
@@ -53,6 +57,45 @@ data['PavedDrive'][data['PavedDrive'] == 'Y'] = 3
 data['PavedDrive'][data['PavedDrive'] == 'P'] = 2
 data['PavedDrive'][data['PavedDrive'] == 'N'] = 1
 
-colu = data.columns[(data.isnull().sum() < 50) & (data.isnull().sum() > 0)]
-for i in colu:
-    print(data[colu].isnull().sum())
+# colu = data.columns[(data.isnull().sum() < 50) & (data.isnull().sum() > 0)]
+# for i in colu:
+#    print(data[colu].isnull().sum())
+
+# populate data for Garage related features when house doesn't have a garage
+populate_missing_values(data, 'GarageArea', 0)
+populate_missing_values(data, 'GarageCars', 0)
+data['GarageFinish'][
+  (data.GarageFinish.isnull() is True) & (data.GarageCond == 0)
+  ] = 0
+data['GarageType'][
+  (data.GarageType.isnull() is True) & (data.GarageCond == 0)
+  ] = 0
+data['GarageYrBlt'][
+  (data.GarageYrBlt.isnull() is True) & (data.GarageCond == 0)
+  ] = 0
+
+
+trainX, testX, trainY, testY = train_test_split(data[data.SalePrice.isnull()==False].drop('SalePrice',axis=1),data.SalePrice[data.SalePrice.isnull()==False],test_size=0.30, random_state=2019)
+trainY = np.log(trainY)
+
+print(trainY)
+
+
+model = XGBRegressor(
+  learning_rate=0.001,
+  n_estimators=4600,
+  max_depth=7,
+  min_child_weight=0,
+  gamma=0,
+  subsample=0.7,
+  colsample_bytree=0.7,
+  scale_pos_weight=1,
+  seed=27,
+  reg_alpha=0.00006
+)
+
+model.fit(trainX, trainY)
+y_pred = model.predict(testX)
+y_pred = np.exp(y_pred)
+
+print(rmsle(testY, y_pred))
